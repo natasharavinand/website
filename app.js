26bbc47b078ac8518a766c45ed53947506e53809
natasharavinand <natasharavinand@gmail.com>
2021-03-20 17:19:00 -0700

    update site 3/20

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const favicon = require('express-favicon');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
